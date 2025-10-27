07bc1b85cc97a39908f63bb1996b929071cb17cb
Rodro375 <rodrigoalonsorpgit@gmail.com>
2025-10-26 19:42:14 -0500

    26-10-25

--- Assets/Common/GlobalHelper.cs
+++ Assets/Common/GlobalHelper.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInteractable
+public static class GlobalHelper
 {
-    void Interact();
-    bool CanInteract();
 
 }
--- Assets/Entities/Enviroment/Grave/Grave.cs
+++ Assets/Entities/Enviroment/Grave/Grave.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Grave : MonoBehaviour
+public class Grave : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject interactionIcon;
+    public bool IsTouched = false;
+    public bool IsInRange = false;
+
+    public Animator transition;
+
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.E) && IsInRange == true && CanInteract())
+        {
+            Interact();
+        }
+    }
+    public bool CanInteract()
+    {
+        return !IsTouched;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Interact()
     {
-        
+        if (!CanInteract()) return;
+        IsTouched = true;
+        TouchGrave();
+    }
+
+    private void TouchGrave()
+    {
+        interactionIcon.SetActive(false);
+        StartCoroutine(LoadCredits());
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && CanInteract())
+        {
+            interactionIcon.SetActive(true);
+            IsInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && CanInteract())
+        {
+            interactionIcon.SetActive(false);
+            IsInRange = false;
+        }
+    }
+
+    IEnumerator LoadCredits()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        transition.Rebind();
+
+        yield return new WaitForSeconds(1);
     }
 }
